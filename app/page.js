265bf50397c26caa332ba44bab5cc138bc6771da
mjahto18-ui <mjahto18@gmail.com>
2026-07-05 12:39:48 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -114,7 +114,7 @@ function InfoBox() {
               marginTop: 4
             }}
           >
-            <span style={{ fontSize: 14 }}>🛍️</span>
+            <span style={{ fontSize: 13 }}>🛍️</span>
             <span>One App For Everything</span>
           </div>
 
