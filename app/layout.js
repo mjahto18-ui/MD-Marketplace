c8a293607914afd70ca4b954f99cb679594d0f7e
mjahto18-ui <mjahto18@gmail.com>
2026-07-06 11:57:14 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,20 @@
-import { Cairo } from 'next/font/google'
-const cairo = Cairo({ subsets: ['arabic'], weight: ['400','700'] })
+iimport './globals.css'
+
+export const metadata = {
+  title: 'MD-Marketplace',
+  description: 'متجر إلكتروني للتوصيل',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
-      <body className={cairo.className}>{children}</body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body className="bg-gray-100">{children}</body>
     </html>
   )
 }
+
