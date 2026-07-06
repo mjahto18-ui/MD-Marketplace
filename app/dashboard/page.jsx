94174705b906809a32d38a76354d858a8153c762
mjahto18-ui <mjahto18@gmail.com>
2026-07-06 21:00:28 +0300

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,7 +5,6 @@ import dynamic from 'next/dynamic';
 
 const Map = dynamic(() => import('../../components/Map'), { ssr: false });
 
-
 export default function Dashboard() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
