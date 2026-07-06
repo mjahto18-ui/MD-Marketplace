dff85c6d0fce170b32da3d359fba24bb5c668b9a
mjahto18-ui <mjahto18@gmail.com>
2026-07-06 18:56:30 +0300

    Update route.js

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -13,7 +13,7 @@ export async function POST(req) {
     });
     const sheets = google.sheets({ version: "v4", auth });
     const res = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.SHEET_ID,
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
       range: "Customers!A2:Z",
     });
     const users = res.data.values || [];
