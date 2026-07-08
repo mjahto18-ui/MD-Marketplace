5cf86d2af8dda4b720bd76116cecf5d8aea18efa
mjahto18-ui <mjahto18@gmail.com>
2026-07-08 11:45:34 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  metadataBase: new URL("https://md-marketplace-seven.vercel.app/")
+  metadataBase: new URL("https://md-marketplace-seven.vercel.app/"),
     
   title: "MD-Marketplace | One App For Everything",
   description: "One App For Everything",
