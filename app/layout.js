2d7aa86f8987b2a9abcebe37ac73339a95a57a79
mjahto18-ui <mjahto18@gmail.com>
2026-07-09 18:10:33 +0300

    Fix formatting of OneSignal appId in layout.js

--- app/layout.js
+++ app/layout.js
@@ -62,7 +62,7 @@ export default function RootLayout({ children }) {
               window.OneSignal = window.OneSignal || [];
               OneSignal.push(function() {
                 OneSignal.init({
-                  appId: "8736bcd3-452e-4b06-a3c1-0363071f1254",
+                  appId: 8736bcd3-452e-4b06-a3c1-0363071f1254,
                   allowLocalhostAsSecureOrigin: true
                 });
               });
