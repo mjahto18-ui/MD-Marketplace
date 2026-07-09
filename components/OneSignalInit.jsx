39b9b2ecab5354b4c3e199a8e9a82720578b047b
mjahto18-ui <mjahto18@gmail.com>
2026-07-09 23:00:33 +0300

    Create OneSignalInit.jsx

---
+++ components/OneSignalInit.jsx
@@ -0,0 +1,17 @@
+"use client";
+import { useEffect } from "react";
+
+export default function OneSignalInit() {
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.OneSignal) {
+      window.OneSignalDeferred = window.OneSignalDeferred || [];
+      window.OneSignalDeferred.push(async function(OneSignal) {
+        await OneSignal.init({
+          appId: "8736bcd3-452e-4b06-a3c1-0363071f1254",
+        });
+      });
+    }
+  }, []);
+
+  return null;
+}
