3750d42e5ce1185013a89bc735077599357ce8ae
mjahto18-ui <mjahto18@gmail.com>
2026-07-09 22:48:52 +0300

    Update NotificationPopup.js

--- components/NotificationPopup.js
+++ components/NotificationPopup.js
@@ -3,17 +3,9 @@ async function enableNotifications() {
 
   alert("نستخدم الإشعارات لتحسين تجربتك داخل التطبيق، ولإعلامك بحالة طلباتك، التوصيل، والعروض الجديدة.");
 
-  // ⭐ ننتظر OneSignal ليجهز بالكامل
-  await OneSignal.User.waitUntilReady();
-
-  // ⭐ هلق منطلب الإذن
   const permission = await OneSignal.Notifications.requestPermission();
 
   if (permission === "granted") {
-
-    // ⭐ ننتظر الـ Service Worker يعمل registration كامل
-    await OneSignal.User.waitUntilReady();
-
     const subId = await OneSignal.User.getSubscriptionId();
 
     console.log("SUB ID:", subId);
