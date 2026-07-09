c386d431d4bbad92e75a2e8fe985454fa2592735
mjahto18-ui <mjahto18@gmail.com>
2026-07-09 23:06:49 +0300

    Update OneSignalInit.jsx

--- components/OneSignalInit.jsx
+++ components/OneSignalInit.jsx
@@ -5,7 +5,7 @@ export default function OneSignalInit() {
   useEffect(() => {
     if (typeof window !== "undefined" && window.OneSignal) {
       window.OneSignalDeferred = window.OneSignalDeferred || [];
-      window.OneSignalDeferred.push(async function(OneSignal) {
+      window.OneSignalDeferred.push(async function (OneSignal) {
         await OneSignal.init({
           appId: "8736bcd3-452e-4b06-a3c1-0363071f1254",
         });
