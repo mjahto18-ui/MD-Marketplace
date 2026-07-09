9d9a2fab9f0f265a3920980ef6fd958f6767759e
mjahto18-ui <mjahto18@gmail.com>
2026-07-09 23:13:54 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,62 +2,16 @@ import "./globals.css";
 import Script from "next/script";
 import OneSignalInit from "@/components/OneSignalInit";
 
-export const metadata = {
-  metadataBase: new URL("https://md-marketplace-seven.vercel.app/"),
-  title: "MD-Marketplace | One App For Everything",
-  description: "One App For Everything",
-  applicationName: "MD-Marketplace",
-  keywords: [
-    "MD Marketplace",
-    "Marketplace",
-    "Delivery",
-    "Lebanon",
-    "Tripoli",
-    "Wallet",
-    "Rewards",
-    "Tracking"
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/logo.png",
-  },
-  openGraph: {
-    title: "MD-Marketplace",
-    description: "One App For Everything",
-    url: "https://md-marketplace-seven.vercel.app/",
-    siteName: "MD-Marketplace",
-    images: [
-      {
-        url: "/logo.png",
-        width: 512,
-        height: 512,
-        alt: "MD-Marketplace Logo",
-      },
-    ],
-    locale: "ar_LB",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "MD-Marketplace",
-    description: "One App For Everything",
-    images: ["/logo.png"],
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <body>
 
-        {/* تحميل SDK */}
-        <Script
+        <Script 
           src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
           strategy="afterInteractive"
         />
 
-        {/* تفعيل OneSignal داخل useEffect */}
         <OneSignalInit />
 
         {children}
