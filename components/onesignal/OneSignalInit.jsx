d071ffb03e5007c01d3def2acbd902b3dbdf2e47
mjahto18-ui <mjahto18@gmail.com>
2026-07-10 15:01:02 +0300

    Update OneSignalInit.jsx

--- components/onesignal/OneSignalInit.jsx
+++ components/onesignal/OneSignalInit.jsx
@@ -10,13 +10,19 @@ export default function OneSignalInit() {
         strategy="afterInteractive"
       />
 
-      <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-<script>
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-      appId: "8736bcd3-452e-4b06-a3c1-0363071f1254",
-    });
-  });
-</script>
+      <Script id="onesignal-init" strategy="afterInteractive">
+        {`
+          window.OneSignalDeferred = window.OneSignalDeferred || [];
+          OneSignalDeferred.push(async function(OneSignal) {
+            await OneSignal.init({
+              appId: "8736bcd3-452e-4b06-a3c1-0363071f1254",
+              allowLocalhostAsSecureOrigin: true
+            });
 
+            console.log("OneSignal جاهز بالكامل");
+          });
+        `}
+      </Script>
+    </>
+  );
+}
