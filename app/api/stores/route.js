047dc0b85fc2747a8258d8f48fbad0e05413468f
mjahto18-ui <mjahto18@gmail.com>
2026-07-13 00:11:53 +0300

    Update route.js

--- app/api/stores/route.js
+++ app/api/stores/route.js
@@ -20,7 +20,7 @@ export async function GET() {
     // ============================
     const storesRes = await sheets.spreadsheets.values.get({
       spreadsheetId,
-      range: "Stores!A:Z",
+      range: "Stores!A:O",
     });
 
     const rows = storesRes.data.values || [];
