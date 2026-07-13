961cbc72555c3786134d02e3bdce5741a566f149
mjahto18-ui <mjahto18@gmail.com>
2026-07-13 11:58:39 +0300

    Update page.js

--- app/order-success/page.js
+++ app/order-success/page.js
@@ -1,6 +1,8 @@
-export const dynamic = "force-dynamic";
 
 'use client';
+
+export const dynamic = "force-dynamic";
+
 import { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 
