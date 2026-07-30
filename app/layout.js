b062a55920b60e93b6a43066214baf324b610012
mjahto18-ui <mjahto18@gmail.com>
2026-07-30 19:24:33 +0300

    Integrate Facebook Pixel tracking in layout
    Added Facebook Pixel tracking script to the layout.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import OneSignalInit from "@/components/onesignal/OneSignalInit";
 import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script';
 
 export const metadata = {
   metadataBase: new URL("https://www.md-marketplace.store"),
@@ -46,6 +47,34 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <body>
+        {/* Facebook Pixel */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1581515696921021');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1581515696921021&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <OneSignalInit />
         {children}
         <Analytics />
