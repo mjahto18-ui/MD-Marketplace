301a5c3abd79e94f2ce045c4712467f1f798cbb1
mjahto18-ui <mjahto18@gmail.com>
2026-07-31 00:12:11 +0300

    Add additional hostnames to next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,22 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.imgur.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
 }
