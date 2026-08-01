1fbdb7c052b679eda4fe2bcf48cf290f696015b7
mjahto18-ui <mjahto18@gmail.com>
2026-08-01 22:31:57 +0300

    Refactor image configuration in next.config.js
    Removed unused image formats and minimum cache TTL settings.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/webp', 'image/avif'], // بيخلي يخدم webp/avif تلقائيا
-    minimumCacheTTL: 31536000, // هيدا الحل للـ 165KiB
     remotePatterns: [
       {
         protocol: 'https',
