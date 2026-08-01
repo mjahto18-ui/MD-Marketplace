2513ebc9ba7ec30dac867926aa0c8d0e50a27c8b
mjahto18-ui <mjahto18@gmail.com>
2026-08-01 22:14:48 +0300

    Enhance image configuration in next.config.js
    Added image formats and minimum cache TTL for optimization.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    formats: ['image/webp', 'image/avif'], // بيخلي يخدم webp/avif تلقائيا
+    minimumCacheTTL: 31536000, // هيدا الحل للـ 165KiB
     remotePatterns: [
       {
         protocol: 'https',
