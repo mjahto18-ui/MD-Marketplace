7e0e9e0cacd854be980309c477bcc84590b6a2d9
mjahto18-ui <mjahto18@gmail.com>
2026-08-25 15:16:36 +0300

    Add 'Bot Session' field to new arrivals response

--- app/api/whatsapp/new-arrivals/route.js
+++ app/api/whatsapp/new-arrivals/route.js
@@ -188,6 +188,7 @@ export async function POST(req) {
     Date: beirutStr,
     "Reassurance_Sent": "YES", // مش YES عشان ما ترجع تفتح
     "Reassurance_At": beirutStr,
+    "Bot Session": "BOT_OFFER"
     Bot: "New Offre",
     "Message Type": "NEW_ARRIVALS",
   }]);
